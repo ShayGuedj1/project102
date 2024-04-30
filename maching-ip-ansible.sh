47bd97afb65e651df146aacab8439f9be2796f65
Shay Guedj <shay.guedj@ibex-ai.com>
2024-04-30 12:30:53 +0300

    .

--- maching-ip-ansible.sh
+++ maching-ip-ansible.sh
@@ -3,6 +3,7 @@
 file1=`cat ip`
 echo $file1
 
+ssh-copy-id -i /home/ubuntu/.ssh/id_rsa.pub ubuntu@$file1
 
 ansible-playbook -i $file1, -u ubuntu ansible/install-services.yaml
 ansible-playbook -i $file1, -u ubuntu ansible/install-website.yaml
