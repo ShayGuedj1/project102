52cee32f9eadfe2b7e6f96725f244b7a01b81664
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-01 21:01:38 +0300

    .

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo 'Starting Terraform: '
+echo 'Starting Terraform:'
 sleep 1
 terraform init
 sleep 3
 pwd
 sleep 3
 echo 'terraform apply'
-terraform apply --auto-approve 
+terraform apply --auto-approve
 sleep 2
 echo 'output of the ip'
 terraform output -json > /home/ubuntu/ips.json
