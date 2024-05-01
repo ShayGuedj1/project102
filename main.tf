426d3fc387c77429accd81585a81355886258b99
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-01 19:38:49 +0300

    .

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "project1" {
   associate_public_ip_address = true  # Assign a public IP to this instance
   key_name      = "projects"
   tags = {
-    Name = "web-server"/*-${count.index + 1}"*/
+    Name = "web-server"
   }
 security_groups = [var.security_groups["docker_sg"]]
 
@@ -27,7 +27,7 @@ provisioner "file" {
 provisioner "remote-exec" {
     inline = [
      "sudo mkdir -p /home/ubuntu/.ssh",  # Create .ssh directory if it doesn't exist
-      "sudo cp /tmp/my-public-key.pub /home/ubuntu/.ssh/authorized_keys",  # Copy public key to authorized_keys
+      "sudo cp /tmp/ansible.pub /home/ubuntu/.ssh/authorized_keys",  # Copy public key to authorized_keys
       "sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh",  # Change ownership to ubuntu user
       "sudo chmod 600 /home/ubuntu/.ssh/authorized_keys"  # Set correct permissions on authorized_keys
     ]
