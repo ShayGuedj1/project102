5cad7838e0179b996e5bbd00110ea8b238c710dd
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-04 16:19:06 +0300

    .

--- main.tf
+++ main.tf
@@ -15,12 +15,12 @@ resource "aws_instance" "project1" {
   connection {
     type        = "ssh"
     user        = "ubuntu"                         # Update with appropriate username
-    private_key = file("/home/projects.pem") # Path to your private key
+    private_key = file("/home/ubuntu/projects.pem") # Path to your private key
     host        = self.public_ip                   # Use the public IP of the instance
   }
 
   provisioner "file" {
-    source      = "/home/jenkins_home/ansible.pub" # Path to your local public key
+    source      = "/home/ubuntu/.ssh/ansible.pub" # Path to your local public key
     destination = "/tmp/ansible.pub"   # Temporary location on the instance
   }
 
