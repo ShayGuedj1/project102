9a0b2d489bf46cee42a74db6a0a7fce885a8f475
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-09 19:36:03 +0300

    initials

--- convert-ip.sh
+++ convert-ip.sh
@@ -1,6 +1,6 @@
 #1/bin/bash
 
-sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/master
+#sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/master
 echo 'output of the ip'
 terraform output -json > /home/ubuntu/ips.json
 cat /home/ubuntu/ips.json #ips.json is the file that copy the IP into the inventory.
