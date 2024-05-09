e184d14d36b1375100c38bd72cac705aef2b287a
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-09 20:21:17 +0300

    initials

--- main.tf
+++ main.tf
@@ -75,21 +75,48 @@ resource "aws_security_group" "project-sg" {
 }
 
 resource "aws_vpc" "project-vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "project"
+  }
 }
 
-resource "aws_subnet" "project-subnet" {
+# Create Internet Gateway
+resource "aws_internet_gateway" "project-igw" {
+  vpc_id = aws_vpc.project-vpc.id
+}
+
+resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.project-vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "project-subnet2" {
+resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.project-vpc.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.project-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.project-igw.id
+  }
+}
+
+resource "aws_route_table_association" "subnet1-association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "subnet2-association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_lb" "project-lb" {
