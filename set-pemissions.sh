f9a1b3c2d4121b1bf2131ffcc3b2cf650f8e86bf
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-09 19:21:24 +0300

    initials

--- set-pemissions.sh
+++ set-pemissions.sh
@@ -4,16 +4,16 @@
 echo 'Starting Terraform:'
 sleep 1
 sudo chown jenkins:jenkins /home/ubuntu/.ssh/master
-sh 'sudo touch /home/ubuntu/ips.json'
-sh 'sudo touch /home/ubuntu/inventory'
-sh 'sudo touch /home/ubuntu/hosts'
-sh 'sudo chmod 777 /home/ubuntu/hosts'
-sh 'sudo chmod 777 /home/ubuntu/ips.json'
-sh 'sudo chmod 777 /home/ubuntu/.ssh'
-sh 'sudo chmod 777 set-ip-ansible.py'
-sh 'sudo chmod 777 install.sh'
-sh 'sudo chmod 777 /home/ubuntu/inventory'
-sh 'sudo chmod 777 convert-ip.sh'
+sudo touch /home/ubuntu/ips.json
+sudo touch /home/ubuntu/inventory
+sudo touch /home/ubuntu/hosts
+sudo chmod 777 /home/ubuntu/hosts
+sudo chmod 777 /home/ubuntu/ips.json
+sudo chmod 777 /home/ubuntu/.ssh
+sudo chmod 777 set-ip-ansible.py
+sudo chmod 777 install.sh
+sudo chmod 777 /home/ubuntu/inventory
+sudo chmod 777 convert-ip.sh
 terraform init
 
 
