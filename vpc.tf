590b2868f190d72a8c99634dae0a605e3e56c8e1
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-11 21:02:37 +0300

    sg and vpc

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "project-vpc" {
     Name = "project-vpc"  # Optional: Set a name tag for your VPC
   }
 }
+
+resource "aws_subnet" "project" {
+  vpc_id     = aws_vpc.project-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "project"
+  }
+}
