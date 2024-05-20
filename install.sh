9a0a28804e546e13694ae9ff77c9091685a8ecf0
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-20 13:42:11 +0300

    home dir as new_home

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 
 echo 'Starting Terraform:'
 sleep 1
-sudo chown jenkins:jenkins /home/ubuntu/.ssh/master
+#sudo chown jenkins:jenkins /home/new_home/.ssh/master
 terraform init
 sleep 3
 echo 'terraform apply'
 terraform apply --auto-approve
 sleep 2
-sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/master.pem
+#sudo chown ubuntu:ubuntu /home/new_home/.ssh/master
 echo 'output of the ip'
-terraform output -json > /home/ubuntu/ips.json
+terraform output -json > /home/new_home/ips.json
 
-cat /home/ubuntu/ips.json #ips.json is the file that copy the IP into the inventory.
+cat /home/new_home/ips.json #ips.json is the file that copy the IP into the inventory.
